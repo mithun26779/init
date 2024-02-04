a55c41a0253a73ef7e7135700c94f51f4e6d60e7
mithun26779 <mithun26779@gmail>
2024-02-04 21:30:08 +0530

    one

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mithun26779/init
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+
+func init() {
+
+	fmt.Println("hello init")
+
+}
+
+func main() {
+
+	fmt.Println("hello main")
+
+}
+
+
+
+
+
+
+
